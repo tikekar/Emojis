6628571975c566303b91891df89148e48f34bf12
Gauri Tikekar <gauritikekar@gauris-mbp.lan>
2016-11-08 18:09:59 -0800

    Initial Commit

---
+++ Emojis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Emojis
+//
+//  Created by Gauri Tikekar on 11/8/16.
+//  Copyright © 2016 Gauri Tikekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Emojis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emojis
+//
+//  Created by Gauri Tikekar on 11/8/16.
+//  Copyright © 2016 Gauri Tikekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
